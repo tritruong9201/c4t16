969e070c115e199949b97d1d25d52d8101589444
tritruong9201 <minhtri9201@gmail.com>
2019-04-13 20:56:02 +0700

    tu hoc bai moi

---
+++ SessionTriTruong/tuhoc.py
@@ -0,0 +1,9 @@
+import random
+n=random.randint(0,3)
+this_list = ["apple", "banana", "orange", "watermelon"]
+print(this_list[n])
+this_tuple = ("lion", "monkey", "tiger", "elephant")
+print(this_tuple[n])
+this_set={"table", "ladder", "chair", "desk"}
+for x in this_set:
+    print(x)
