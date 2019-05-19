e6a9a0ce88e4d843a33ac791e042ff12906eac58
tritruong9201 <minhtri9201@gmail.com>
2019-05-19 23:39:16 +0700

    Bài tập game chỉnh sửa hoàn thiện

--- Session12/gameescape.py
+++ Session12/gameescape.py
@@ -6,6 +6,10 @@ K={
     "X" : 2,
     "Y" : 1,
 }
+K2={
+    "X" : 0,
+    "Y" : 0,
+}
 P={
     "X" : 1,
     "Y" : 2,
@@ -14,45 +18,43 @@ E={
     "X" : 3,
     "Y" : 2,
 }
-key_storage_K=0
-key_storage_E=0
-key_storage_E_1=0
+W={
+    "X" : 2,
+    "Y" : 2,
+}
+key_storage=0
+key_storage_2=0
 while True:
     for y in range(map["Y"]):
         for x in range(map["X"]):
-            P_is_here=False
             if P["X"]==x and P["Y"]==y:
-                P_is_here=True
-            K_is_here=False
-            if K["X"]==x and K["Y"]==y:
-                K_is_here=True
-            if K_is_here:
-                if P["X"]==K["X"] and P["Y"]==K["Y"]:
-                    key_storage_K=1
-            E_is_here=False
-            if E["X"]==x and E["Y"]==y:
-                E_is_here=True
-            if E_is_here:
-                if P["X"]==E["X"] and P["Y"]==E["Y"]:
-                    if key_storage_K==1:
-                        key_storage_E=1
-                    else:
-                        key_storage_E_1=1
-            
-            if P_is_here:
                 print("P",end=" ")
-            elif K_is_here:
-                if key_storage_K==1:
+            elif K["X"]==x and K["Y"]==y:
+                if key_storage==1:
                     print("-",end=" ")
                 else:
-                    print("K",end=" ")    
-            elif E_is_here:
+                    print("K",end=" ") 
+            elif K2["X"]==x and K2["Y"]==y:
+                if key_storage_2==1:
+                    print("-",end=" ")
+                else:
+                    print("K",end=" ")
+            elif E["X"]==x and E["Y"]==y:
                 print("E",end=" ")
+            elif W["X"]==x and W["Y"]==y:
+                print("W",end=" ")
             else:
                 print("-",end=" ")
         print(" ")
-
-    # how to move
+    if P["X"]==E["X"] and P["Y"]==E["Y"]:
+        if key_storage==1:
+            if key_storage_2==1:
+                print("CONGRATS, YOU HAVE JUST ESCAPED THE DUNGEON")
+                break
+            else:
+                print("YOU NEED KEYS TO ESCAPE")
+        else:
+            print("YOU NEED KEYS TO ESCAPE THE DUNGEON")
     yourmove=input("Your move: ").upper()
     dx=0
     dy=0
@@ -67,5 +69,12 @@ while True:
     else:
         print("INVALID")
     if 0<=P["X"]+dx<map["X"] and 0<=P["Y"]+dy<map["Y"]:
-        P["X"]+=dx
-        P["Y"]+=dy
+        if P["X"]+dx!=W["X"] or P["Y"]+dy!=W["Y"]:
+            P["X"]+=dx
+            P["Y"]+=dy
+    if P["X"]==K["X"] and P["Y"]==K["Y"]:
+        key_storage+=1
+        print("YOU HAVE JUST PICKED A KEY")
+    if P["X"]==K2["X"] and P["Y"]==K2["Y"]:
+        key_storage_2+=1
+        print("YOU HAVE JUST PICKED A KEY")
