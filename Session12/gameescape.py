560c8afec997b253b13322efcb2efc3769fc30ff
tritruong9201 <minhtri9201@gmail.com>
2019-05-19 21:17:24 +0700

    BAI TAP GAME

--- Session12/gameescape.py
+++ Session12/gameescape.py
@@ -14,6 +14,9 @@ E={
     "X" : 3,
     "Y" : 2,
 }
+key_storage_K=0
+key_storage_E=0
+key_storage_E_1=0
 while True:
     for y in range(map["Y"]):
         for x in range(map["X"]):
@@ -22,19 +25,27 @@ while True:
                 P_is_here=True
             K_is_here=False
             if K["X"]==x and K["Y"]==y:
-                sum=0
+                K_is_here=True
+            if K_is_here:
                 if P["X"]==K["X"] and P["Y"]==K["Y"]:
-                    sum+=1
-                if sum<1:
-                    K_is_here=True
+                    key_storage_K=1
             E_is_here=False
             if E["X"]==x and E["Y"]==y:
                 E_is_here=True
+            if E_is_here:
+                if P["X"]==E["X"] and P["Y"]==E["Y"]:
+                    if key_storage_K==1:
+                        key_storage_E=1
+                    else:
+                        key_storage_E_1=1
             
             if P_is_here:
                 print("P",end=" ")
             elif K_is_here:
-                print("K",end=" ")
+                if key_storage_K==1:
+                    print("-",end=" ")
+                else:
+                    print("K",end=" ")    
             elif E_is_here:
                 print("E",end=" ")
             else:
@@ -58,4 +69,3 @@ while True:
     if 0<=P["X"]+dx<map["X"] and 0<=P["Y"]+dy<map["Y"]:
         P["X"]+=dx
         P["Y"]+=dy
-    print(P["X"],P["Y"],"-",K["X"],K["Y"])
