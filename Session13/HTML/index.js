370251de964987f8a16866d732eb2b9c4ffedf8e
tritruong9201 <minhtri9201@gmail.com>
2019-06-07 21:28:55 +0700

    Bai tap Java Script

---
+++ Session13/HTML/index.js
@@ -0,0 +1,16 @@
+alert("Hello world");
+console.log("Hello again");
+// var i=0;
+// while(i<3) {
+//     console.log("HELLO");
+//     i++;
+// }
+
+// for (var j=0;j<3;j++) {
+//     console.log("HI")
+// }
+
+var arr=["Captain America","Thor","Doctor Strange","Star Lord","Black Panther"]
+for (var i=0;i<arr.length;i++) {
+    console.log(arr[i])
+}
