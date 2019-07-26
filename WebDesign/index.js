582f31fc3526d2720dceb1b860ae1c82959afbc2
tritruong9201 <minhtri9201@gmail.com>
2019-07-26 13:19:07 +0700

    Web Design

--- WebDesign/index.js
+++ WebDesign/index.js
@@ -1,3 +1,9 @@
+AOS.init();
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+};
+
 var search_value = document.getElementById("search_value");
 
 function SearchFunction() {
